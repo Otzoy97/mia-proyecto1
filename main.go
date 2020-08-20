4d907188d1036097bdfef9981f7add4dbf831fbd
S.Otzoy <otzoy97@hotmail.com>
2020-08-20 17:18:02 -0600

    scanner y parser puestos a prueba

--- main.go
+++ main.go
@@ -3,31 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"mia-proyecto1/analyzer"
 	"os"
+	"strings"
 )
 
 func main() {
-	//src := []byte("mkdisk -path->\"/home/usr/\" -name->disco1 -add->-80\n")
+	//line := []byte("mkdisk -path->/home/usr\n")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Preaparado")
+	auxLine := []byte{}
+	aux := ""
+	fmt.Println("Ready")
 	for {
-		if _, err := os.Stdout.WriteString(""); err != nil {
-			log.Fatalf("WriteString: %s", err)
-		}
+	LeerEntrada:
+		//Lee la entrada del usuario
 		line, err := in.ReadBytes('\n')
-		if err == io.EOF {
-			return
-		}
 		if err != nil {
-			log.Fatalf("ReadBytes: %s", err)
+			log.Fatalf("Error al leer entrada del usuario: %s", err)
 		}
-		lex := analyzer.Lexer{Line: line}
+		//Convierte line a una cadena y
+		//asi poder realizar operaciones entre cadenas
+		aux = string(line)
+		aux = strings.TrimSpace(aux)
+		//Une la entrada del usuario a auxLine
+		auxLine = append(auxLine, line...)
+		//Si en line viene los caracteres '\*'
+		//regresa a LeerEntrada para leer otra entrada del usuario
+		if len(aux) > 2 && aux[len(aux)-2:] == "\\*" {
+			goto LeerEntrada
+		}
+		//Analiza los bytes almacenados en auxLine
+		lex := analyzer.Lexer{Line: auxLine}
 		lex.Scanner()
+		analyzer.Parser()
+		//Reinicia auxLine
+		auxLine = nil
 	}
-	//lex := analyzer.Lexer{Line: src}
-	//lex.Scanner()
-
 }
