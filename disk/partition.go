b16485e4700a8615b34c5b17c92e07d806f920e0
S.Otzoy <otzoy97@hotmail.com>
2020-08-24 02:56:21 -0600

    se implementó sort.Interface

--- disk/partition.go
+++ disk/partition.go
@@ -6,3 +6,10 @@ type Partition struct {
 	PartStart, PartSize           uint32
 	PartName                      [16]byte
 }
+
+//ByPartStart sort.Interface basado en el campo PartStart
+type ByPartStart []Partition
+
+func (a ByPartStart) Len() int           { return len(a) }
+func (a ByPartStart) Less(i, j int) bool { return a[i].PartStart < a[j].PartStart }
+func (a ByPartStart) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
