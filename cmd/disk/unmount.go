19d03cd6855330a5955efb42cc63b50a0a44fea0
S.Otzoy <otzoy97@hotmail.com>
2020-08-25 19:26:59 -0600

    cambios mínimos

--- cmd/disk/unmount.go
+++ cmd/disk/unmount.go
@@ -9,19 +9,19 @@ import (
 //Unmount ...
 type Unmount struct {
 	Row   int
-	idlst []string
+	Oplst []string
 }
 
 //AddOp agrega un nuevo id, si no es un id lo rechaza
 func (m *Unmount) AddOp(k string, v interface{}) {
 	if "id" == k {
-		m.idlst = append(m.idlst, v.(string))
+		m.Oplst = append(m.Oplst, v.(string))
 	}
 }
 
 //Validate ...
 func (m *Unmount) Validate() bool {
-	if len(m.idlst) > 0 {
+	if len(m.Oplst) > 0 {
 		return true
 	}
 	color.New(color.FgHiYellow).Printf("Unmount no se puede ejecutar (%v)\n", m.Row)
@@ -30,7 +30,7 @@ func (m *Unmount) Validate() bool {
 
 //Run desmonta todas las particiones listadas
 func (m *Unmount) Run() {
-	for _, id := range m.idlst {
+	for _, id := range m.Oplst {
 		disk.RmImg(id)
 	}
 }
