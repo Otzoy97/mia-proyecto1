ba3c83dbbf818e715005aad876442fd7f1491087
S.Otzoy <otzoy97@hotmail.com>
2020-09-09 01:12:39 -0600

    CreateBitMap arreglado y testeado

--- cmd/rep/bitmap.go
+++ cmd/rep/bitmap.go
@@ -1,20 +1,21 @@
 package rep
 
-import "strings"
-
 //CreateBitMap crea el reporte del bitmap
 func (r *Rep) CreateBitMap(bitmap []byte) []byte {
-	var strD strings.Builder
+	var b []byte
+	//Sirve para controlar el número de bytes en una
+	//sola línea|
 	cte := 0
+	//Construye un array de bytes con el bitmap
 	for _, v := range bitmap {
 		if cte == 20 {
-			strD.WriteByte('\n')
+			//Agrega un salto de espacio luego de 20 bytes
+			b = append(b, '\n')
 			cte = 0
-		} else {
-			strD.WriteByte(v + '0')
-			strD.WriteByte('\t')
-			cte++
 		}
+		//Coloca la versión numérica del valor 'v'
+		b = append(b, v+'0', '\t')
+		cte++
 	}
-	return []byte(strD.String())
+	return b
 }
